3dce1ccee26bfb78b9ce66fa10d061cc953be270
walker-sean <walker.se@northeastern.edu>
2023-08-31 17:21:59 -0400

    Testing out UserDefaults and AppStoarge

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,35 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
-        NavigationView {
-            VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                Button("Add Number") {
-                    withAnimation {
-                        numbers.append(currentNumber)
-                    }
-                    currentNumber += 1
-                }
-            }
-            .navigationTitle("onDelete")
-            .toolbar {
-                EditButton()
-            }
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
     }
-    
-    func removeRows(at offsets: IndexSet) {
-            numbers.remove(atOffsets: offsets)
-        }
 }
 
 struct ContentView_Previews: PreviewProvider {
