4c2d6e74d69df53cccde474ac55d0272130b67ba
walker-sean <walker.se@northeastern.edu>
2023-08-31 12:40:20 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sean Walker on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
